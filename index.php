cbbae82e3d945f0eefb098fd0f89ab8590e6b501
DhakerSalah <dhaker.saleh@clevertech-france.fr>
2022-03-03 18:51:12 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php 
+	include("config.php");
+?>
+<html>
+<head>
+	<title>D'S-DEV</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+	<script>
+	$(document).ready(function(){
+		load_data();
+		function load_data(query)
+		{
+			$.ajax({
+			url:"searchresult.php",
+			method:"POST",
+			data:{query:query},
+			success:function(data)
+			{
+				$('#result').html(data);
+			}
+			});
+		}
+		$('#search').keyup(function(){
+		var search = $(this).val();
+		if(search != '')
+		{
+			load_data(search);
+		}
+		else
+		{
+			load_data();
+		}
+		});
+	});
+	</script>
+</head>
+<body>
+	<div class="container">
+		<h1 class="text-center text-primary">Filter data in table using jQuery PHP & MySQL</h1>
+		<hr>
+		<input type="text" name="search" id="search" placeholder="Search" class="form-control" />
+		<div id="result"></div>
+	</div>
+</body>
+</html>
